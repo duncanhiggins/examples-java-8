3993babea26c2acab747ad83d9836541c38e63e0
Duncan Higgins <mail@duncan-higgins.org>
2019-10-16 03:14:43 +0100

    Add new test files.

---
+++ src/test/java/java/util/function/IntFunctionTest.java
@@ -0,0 +1,15 @@
+package java.util.function;
+
+import org.junit.Test;
+
+public class IntFunctionTest {
+
+    @Test
+    public void basicTest() {
+
+        IntFunction f = x -> x + 1;
+        
+        
+    }
+
+}
